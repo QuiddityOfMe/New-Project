ab9d30b96c6bfd414353adcf0d41f88994f13177
Camilla Hawley <camillasarahawley@gmail.com>
2019-10-10 18:46:13 -0500

    Enemies, Health, Script changes
    Added enemies and damagables. Added collectible health. Added new scripts, and further edited scripts.

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,19 +4,59 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-  // Start is called before the first frame update
+    public float playerSpeed = 3.0f;
+
+    public int maxHealth = 5;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
+
+    Rigidbody2D rigidbody2d;
+
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //Bind keyboard input to horizontal and vertical axes
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.x + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+
+        //Move player 3 units per second
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + playerSpeed * horizontal * Time.deltaTime;
+        position.y = position.y + playerSpeed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+    }
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        //Changes player's health
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
